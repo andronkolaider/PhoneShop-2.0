4dfe78766b5156a5eb5591955daf8f930320302a
Roman Martyniuk <hjbfrcgq@gmail.com>
2018-11-01 14:50:57 +0200

    OrderContractService created

---
+++ PhoneShopSolution/PhoneShop.WCF/MockModuls/MockData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneShop.WCF.MockModuls {
+    public static class MockData {
+
+        public static List<Models.UserDTO> GetUsersDTO() {
+
+            return new List<Models.UserDTO> {
+
+                new Models.UserDTO { Id = 0, Name="Andrii",     Email="gaypornhere@gmail.com"},
+                new Models.UserDTO { Id = 1, Name="Vitalii",    Email="fancytoys@gmail.com"},
+                new Models.UserDTO { Id = 2, Name="Gabe",       Email="getblowjob@gmail.com"},
+
+            };
+
+        }
+
+        public static List<Models.PhoneDTO> GetPhonesDTO() {
+
+            return new List<Models.PhoneDTO> {
+
+                new Models.PhoneDTO { Id = 0, Brand="Apple",    Model="IPhone 6s"},
+                new Models.PhoneDTO { Id = 1, Brand="Google",   Model="Pixel"},
+                new Models.PhoneDTO { Id = 2, Brand="Samsung",  Model="Galaxy S9"},
+
+            };
+
+        }
+
+
+    }
+}
---
+++ PhoneShopSolution/PhoneShop.WCF/Models/PhoneDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneShop.WCF.Models {
+
+    [DataContract]
+    public class PhoneDTO {
+
+        [DataMember]
+        public int      Id        { set; get; }
+        [DataMember]
+        public string   Brand     { set; get; }
+        [DataMember]
+        public string   Model     { set; get; }
+        [DataMember]
+        public double   Price     { set; get; }
+
+    }
+}
